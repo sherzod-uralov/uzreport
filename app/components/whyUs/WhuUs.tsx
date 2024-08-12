3969a7fa75bb9e724c97e351e40f9b66c6692173
Abdulaziz Abdullayev <108785546+Abdulaziz2003333@users.noreply.github.com>
2024-08-12 18:42:30 +0500

    update cards

--- app/components/whyUs/WhuUs.tsx
+++ app/components/whyUs/WhuUs.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslations } from "next-intl";
-
+import whyImg from "@/public/assets/whyUs.svg";
+import Image from "next/image";
 const WhuUs = () => {
   const t = useTranslations("WhyUs");
 
@@ -27,11 +28,19 @@ const WhuUs = () => {
 
   return (
     <section className="container">
-      <div>
-        <h1 className="text-[40px] font-bold leading-[53px] tracking-[-1px] text-[#042552]">
-          {t("title")}{" "}
-        </h1>
-        {t("reasons.0")}
+      <div className="flex justify-between ">
+        <div>
+          <h1 className=" text-lg font-semibold leading-[25px] text-[#46658B]">
+            {t("title")}
+          </h1>
+
+          <Image src={whyImg} alt="img" className="w-[620px] h-[580px]" />
+        </div>
+        <div>
+          <h1 className="text-[40px] font-bold leading-[53px] tracking-[-1px] text-[#042552]">
+            {t("reasons.0")}
+          </h1>
+        </div>
       </div>
     </section>
   );
