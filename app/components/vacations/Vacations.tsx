f9459e1a164481c6d8fa7cb29af953c2736571be
sherzod-uralov <sherzoduralov432@gmail.com>
2024-08-13 14:52:39 +0500

    add gsap animation

--- app/components/vacations/Vacations.tsx
+++ app/components/vacations/Vacations.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import dots from "@/public/assets/dots.svg";
 import { useTranslations } from "next-intl";
@@ -10,25 +12,55 @@ import cardImg5 from "@/public/assets/computer_white_on_transparent 1.svg";
 import cardImg6 from "@/public/assets/user_generated_content_white_on_transparent 1.svg";
 import cardImg7 from "@/public/assets/music_white_on_transparent 1.svg";
 import cardImg8 from "@/public/assets/social_media_white_on_transparent 1.svg";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Vacations = () => {
   const t = useTranslations("vacations");
-  console.log(t("cards"));
+  const cardsRef = useRef([]);
+
+  useEffect(() => {
+    cardsRef.current.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          opacity: 0,
+          y: 100,
+        },
+        {
+          opacity: 1,
+          transition: 0.5,
+          y: 0,
+          duration: 0.2,
+          ease: "sine",
+          scrollTrigger: {
+            trigger: card,
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
+          delay: index * 0.2,
+        },
+      );
+    });
+  }, []);
+
   return (
     <section id="work" className="pt-[360px] max-sm:pt-20">
-      <article className="container ">
-        <div className="flex justify-center items-center mt-5 mb-[74px] ">
+      <article className="container">
+        <div className="flex justify-center items-center mt-5 mb-[74px]">
           <Image
             src={dots}
             alt="dots"
             className="left-0 absolute w-[70px] max-xl:hidden"
           />
-          <h1 className=" text-[30px] font-bold leading-[53px] tracking-[-1px] text-center max-sm:text-[17px] max-sm:leading-[20px] max-sm:tracking-normal max-lg:text-xl ">
-            {t("title1")} <span className="text-[#3D9386]">{t("title2")} </span>{" "}
+          <h1 className="text-[30px] font-bold leading-[53px] tracking-[-1px] text-center max-sm:text-[17px] max-sm:leading-[20px] max-sm:tracking-normal max-lg:text-xl">
+            {t("title1")} <span className="text-[#3D9386]">{t("title2")}</span>{" "}
             {t("title3")}
           </h1>
         </div>
-        <div className="flex  flex-wrap  gap-3 justify-center  ">
+        <div className="flex flex-wrap gap-3 justify-center">
           {[
             cardImg1,
             cardImg2,
@@ -39,20 +71,22 @@ const Vacations = () => {
             cardImg7,
             cardImg8,
           ].map((i, e) => (
-            <div className="flex flex-grow border border-solid border-[#EBEFF6] w-[300px]  rounded-md  items-center max-sm:w-full  hover:shadow-[1px_0px_5px_3px_rgba(56,196,160,0.6)] transition ease-in-out delay-150  ">
+            <div
+              key={e}
+              className="flex flex-grow border border-solid border-[#EBEFF6] w-[300px] rounded-md items-center max-sm:w-full hover:shadow-[1px_0px_5px_3px_rgba(56,196,160,0.6)] transition ease-in-out delay-150"
+              ref={(el: never) => (cardsRef.current[e] = el)}
+            >
               <div
                 style={{ backgroundColor: t(`cards.${e}.cardColor`) }}
                 className="w-[60px] h-[60px] flex justify-center items-center ml-[30px] mt-[30px] mb-[30px] rounded-[10px]"
               >
                 <Image src={i} alt="" />
               </div>
-              <div className=" ml-[20px]">
+              <div className="ml-[20px]">
                 <h1 className="text-lg font-bold leading-[26px]">
-                  {" "}
                   {t(`cards.${e}.title`)}
                 </h1>
                 <p className="text-base font-normal leading-7">
-                  {" "}
                   {t(`cards.${e}.slots`)}
                 </p>
               </div>
--- app/components/whyUs/WhuUs.tsx
+++ app/components/whyUs/WhuUs.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import { useTranslations } from "next-intl";
 import whyImg from "@/public/assets/whyUs.svg";
 import checkmark from "@/public/assets/checkmark.svg";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const WhuUs = () => {
   const t = useTranslations("WhyUs");
+
+  const imageRef = useRef(null);
+  const textRefs = useRef([]);
 
   const whyUs = [
     {
@@ -24,11 +34,48 @@ const WhuUs = () => {
     },
   ];
 
+  useEffect(() => {
+    gsap.fromTo(
+      imageRef.current,
+      { opacity: 0, x: -100 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: imageRef.current,
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+      },
+    );
+
+    textRefs.current.forEach((textRef, index) => {
+      gsap.fromTo(
+        textRef,
+        { opacity: 0, x: 100 },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 1,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: textRef,
+            start: "top 80%",
+            toggleActions: "play none none reverse",
+          },
+          delay: index * 0.2,
+        },
+      );
+    });
+  }, []);
+
   return (
     <section className="mt-[150px] max-lg:mt-12">
       <article className="container">
         <div className="flex justify-between max-lg:flex-col">
-          <div>
+          <div ref={imageRef}>
             <Image
               src={whyImg}
               alt="img"
@@ -43,9 +90,13 @@ const WhuUs = () => {
               <div className="flex max-lg:items-center max-sm:items-start flex-col items-start gap-[38px]">
                 {whyUs.map((item, index) => (
                   <div
-                    className={`${index % 2 === 0 ? "lg:pl-20" : "pl-0"} flex gap-[39px] items-center justify-between`}
+                    key={index}
+                    ref={(el: never) => (textRefs.current[index] = el)}
+                    className={`${
+                      index % 2 === 0 ? "lg:pl-20" : "pl-0"
+                    } flex gap-[39px] items-center justify-between`}
                   >
-                    <Image src={checkmark} alt={index + "-image"} />
+                    <Image src={checkmark} alt={`${index}-image`} />
                     <h2 className="max-sm:text-[18px] text-[#042552] font-[500] text-[22px]">
                       {item.title}
                     </h2>
